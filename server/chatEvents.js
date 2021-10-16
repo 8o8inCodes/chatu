7b23286d6c6216c4e6397ed160fbffdd0b1831c3
Leonid Haimov <8o8inSquares@gmail.com>
2021-10-16 16:54:32 +0300

    Add clueless bot

--- server/chatEvents.js
+++ server/chatEvents.js
@@ -1,4 +1,6 @@
 const shortid = require('shortid');
+const ChatuBotu = require('./chatubotu');
+const bot = new ChatuBotu();
 
 const setupChatEvents = (io) => {
     io.on('connection', (socket) => {
@@ -16,6 +18,19 @@ const setupChatEvents = (io) => {
           ...message,
           id: shortid.generate()
         });
+
+        if(message?.message.trim().endsWith("?")){
+          const answer = bot.findPossibleAnswer(message.message);
+          if(answer){
+            io.emit('message', {
+              id: shortid.generate(),
+              author: bot.name,
+              nameColor: bot.color,
+              message: answer,
+              badges: []
+            });
+          }
+        }
       })
     });
   };
